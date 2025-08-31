d807b8ade2c516709af5229315d8578a81289071
EthLeigh <ethleigh@proton.me>
2025-08-31 23:03:14 +0200

    Update mod info to reflect DST compatibility (not doing rn)

--- modinfo.lua
+++ modinfo.lua
@@ -3,10 +3,10 @@
 name = "Don't Starve Neuro"
 version = "0.0.0"
 author = "EthLeigh"
-description = "Allows Neuro-sama (or Evil) to play Don't Starve and Don't Starve Together."
+description = "Allows Neuro-sama (or Evil) to play Don't Starve."
 
 dont_starve_compatible = true
-dst_compatible = true
+dst_compatible = false
 all_clients_require_mod = false
 client_only_mod = true
 reign_of_giants_compatible = true
