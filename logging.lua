9af82d144982f072ca4462f748137a29cc16645a
EthLeigh <ethleigh@proton.me>
2025-11-16 20:24:16 +0200

    Add logging function for printing table keys and values

--- logging.lua
+++ logging.lua
@@ -12,3 +12,14 @@ end
 function log_error(...)
     print("[DSN] [ERROR]", ...)
 end
+
+---@param table table<string, any>
+function log_entries(table)
+    log_info("Started logging entries for: ", table)
+
+    for k, v in pairs(table) do
+        log_info(k, v)
+    end
+
+    log_info("Stopped logging entries for: ", table)
+end
