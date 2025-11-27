a97f29bb93469b53422e8d16c81e51b164ff8c30
EthLeigh <ethleigh@proton.me>
2025-11-27 19:22:42 +0200

    Fix the No longer starving message sending after automatically eating

--- managers/context_manager.lua
+++ managers/context_manager.lua
@@ -1,16 +1,18 @@
 ContextManager = {}
 
-ContextManager._sentForDarkness = false
+ContextManager._sent_for_darkness = false
+
+ContextManager._debounce_starve = true
 
 local sent_season_update = false
 
 --- Checks if the player is in/leaving darkness and sends context
 local function HandleCheckPlayerEnterDarkness()
-    if PlayerLightWatcher:IsInLight() and ContextManager._sentForDarkness then
-        ContextManager._sentForDarkness = false
+    if PlayerLightWatcher:IsInLight() and ContextManager._sent_for_darkness then
+        ContextManager._sent_for_darkness = false
         ApiBridge.HandleSendContext("Your character is no longer in the dark.", true)
-    elseif not PlayerLightWatcher:IsInLight() and not ContextManager._sentForDarkness then
-        ContextManager._sentForDarkness = true
+    elseif not PlayerLightWatcher:IsInLight() and not ContextManager._sent_for_darkness then
+        ContextManager._sent_for_darkness = true
         ApiBridge.HandleSendContext("Your character is in the dark, run to the nearest light source or " ..
             "make one. Your character will lose sanity in the dark and get attacked by shadow monsters.")
     end
@@ -20,9 +22,12 @@ end
 ---@param food_eaten string | nil
 function ContextManager.HandlePlayerStarve(food_eaten)
     if food_eaten then
-        -- TODO: Still triggers the 'no longer starving' message
+        ContextManager._debounce_starve = true
+
         ApiBridge.HandleSendContext("Your character started starving but ate food to stay fed.", true)
     else
+        ContextManager._debounce_starve = false
+
         ApiBridge.HandleSendContext("Your character is starving and will take damage until you " ..
             "eat but there is no food in your character's inventory.")
     end
@@ -30,6 +35,12 @@ end
 
 --- Handles the event for when the player starts starving
 local function HandlePlayerStopStarve()
+    if ContextManager._debounce_starve then
+        ContextManager._debounce_starve = false
+
+        return
+    end
+
     ApiBridge.HandleSendContext("Your character is no longer starving.", true)
 end
 
