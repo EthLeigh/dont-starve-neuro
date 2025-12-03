60e1d65d07e9fe0475c2697d62eabff305485b0f
EthLeigh <ethleigh@proton.me>
2025-12-03 18:28:55 +0200

    Reorder Util get action for entity to prioritize more common actions

--- utils.lua
+++ utils.lua
@@ -7,14 +7,14 @@ modimport("classes/action_queue.lua")
 function Utils.GetActionForEntity(ent)
     if ent == nil then return end
 
-    if ent.components.pickable and ent.components.pickable:CanBePicked() then
+    if ent.components.inventoryitem and ent.components.inventoryitem.canbepickedup then
+        return "pickup", GLOBAL.ACTIONS.PICKUP
+    elseif ent.components.pickable and ent.components.pickable:CanBePicked() then
         return "pick", GLOBAL.ACTIONS.PICK
     elseif ent.components.harvestable and ent.components.harvestable:CanBeHarvested() then
         return "harvest", GLOBAL.ACTIONS.HARVEST
     elseif ent.components.crop and ent.components.crop:IsReadyForHarvest() then
         return "harvest", GLOBAL.ACTIONS.HARVEST
-    elseif ent.components.inventoryitem and ent.components.inventoryitem.canbepickedup then
-        return "pickup", GLOBAL.ACTIONS.PICKUP
     elseif ent.components.inspectable then
         -- TODO: Add a context message for the character response
         return "examine", GLOBAL.ACTIONS.LOOKAT
