8a5504cc2743373b74ab94070638395d2fba06b0
EthLeigh <ethleigh@proton.me>
2025-12-10 20:11:40 +0200

    Fix incorrect action result command key

--- bridge/src/constants/outgoingMessageActions.ts
+++ bridge/src/constants/outgoingMessageActions.ts
@@ -3,7 +3,7 @@ export const CONTEXT_ACTION = 'context';
 export const ACTIONS_REGISTER_ACTION = 'actions/register';
 export const ACTIONS_UNREGISTER_ACTION = 'actions/unregister';
 export const ACTIONS_FORCE_ACTION = 'actions/force';
-export const ACTIONS_RESULT_ACTION = 'actions/result';
+export const ACTIONS_RESULT_ACTION = 'action/result';
 export const SHUTDOWN_READY_ACTION = 'shutdown/ready';
 
 export const VALID_ACTION_COMMANDS = [
